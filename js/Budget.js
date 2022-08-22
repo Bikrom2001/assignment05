6049f22b0c1a4b95926e01679dec981ef1b25d97
Bikrom-Roy <bikromroy2001@gmail.com>
2022-08-22 10:36:59 +0600

    adding funcation

--- js/Budget.js
+++ js/Budget.js
@@ -1,13 +1,25 @@
-
-document.getElementById('calculate-btn').addEventListener('click', function(){
-
-    const perPlayerField = document.getElementById('per-player-field');
-    const perPlayerFieldString = perPlayerField.value ;
+function getInputElementById(InputElementId) {
+    const perPlayerField = document.getElementById(InputElementId);
+    const perPlayerFieldString = perPlayerField.value;
     const newPerPlayer = parseFloat(perPlayerFieldString);
     perPlayerField.value = '';
-    
-    const totalPlayerExpenses = newPerPlayer * 5 ;
-    
-    const newTotalExpensesAmount = document.getElementById('Expenses-amount');
-    newTotalExpensesAmount.innerText = totalPlayerExpenses ;
+    return newPerPlayer;
+}
+
+function getElementById(elementId, value) {
+    const newTotalExpensesAmount = document.getElementById(elementId);
+    const newTotalExpensesAmountString = newTotalExpensesAmount.innerText;
+    const newExpensesTotal = parseFloat(newTotalExpensesAmountString);
+    newTotalExpensesAmount.innerText = value;
+    return newExpensesTotal;
+}
+
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    const newPerPlayer = getInputElementById('per-player-field');
+    const totalPlayerExpenses = newPerPlayer * 5;
+    const currentTotalExpensesAmount = getElementById('Expenses-amount', totalPlayerExpenses);
+
 })
