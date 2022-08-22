eb2e7fcd224673262560ef952db8cb044282d556
Bikrom-Roy <bikromroy2001@gmail.com>
2022-08-22 11:10:35 +0600

    Calculate Total end

--- js/Budget.js
+++ js/Budget.js
@@ -15,11 +15,26 @@ function getElementById(elementId, value) {
 }
 
 
-
+// Player Expenses
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
     const newPerPlayer = getInputElementById('per-player-field');
     const totalPlayerExpenses = newPerPlayer * 5;
     const currentTotalExpensesAmount = getElementById('Expenses-amount', totalPlayerExpenses);
 
+})
+
+
+// Calculate Total
+document.getElementById('calculate-total-btn').addEventListener('click', function(){
+    const managerField = getInputElementById('Manager-field');
+    const coachField = getInputElementById('Coach-field');
+
+    const expensesAmount = document.getElementById('Expenses-amount');
+    const expensesAmountString = expensesAmount.innerText ;
+    const currentExpensesAmount = parseFloat(expensesAmountString);
+    
+    const total = currentExpensesAmount + managerField + coachField ;
+    getElementById('total-amount', total);
+    
 })
